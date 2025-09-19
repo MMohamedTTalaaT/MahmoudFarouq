89dbc0ce7c2412f530c85c8f4b2e83fe7e28da08
MMohamed TTalaaT <144382878+MMohamedTTalaaT@users.noreply.github.com>
2025-09-20 01:21:35 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+document.querySelector(".js-showmore").addEventListener("click", () => {
+  document.querySelector(".js-portfolio").classList.toggle("hidden");
+  document.querySelector(".js-portfolio").classList.toggle("flex");
+});
+
+document.querySelector(".js-back-button").addEventListener("click", () => {
+  document.querySelector(".js-portfolio").classList.toggle("hidden");
+  document.querySelector(".js-portfolio").classList.toggle("flex");
+});
